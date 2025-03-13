03cd8cc1400ac02c2a5e881b790adac38bfd9872
Gavish3 <161205236+Gavish3@users.noreply.github.com>
2025-03-13 12:06:23 +0530

    Update SessionUtils.java

--- src/main/java/com/basicBank/practice/util/SessionUtils.java
+++ src/main/java/com/basicBank/practice/util/SessionUtils.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class SessionUtils {
 
     public static String getLoggedInUsername() {
+        //to get the details of logged in user using session id 
+        //sessionId is stored inside security context that we access using SecurityContextHolder.
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
